f88edbfa8162c3bd7876d423d08ec96c419ccc35
Jason Roeckle <jason.roeckle@gmail.com>
2016-02-07 16:12:47 -0800

    End of Chapter 10
    - TODO : Challenges

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,11 +1,25 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent i = new Intent(packageContext, CrimeActivity.class);
+        i.putExtra(EXTRA_CRIME_ID, crimeID);
+        return i;
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
